431a5e2d67bc6eee17d90457b6bc70959ef1c49f
Yevhenii Shapovalov <yshapova@redhat.com>
2018-02-09 17:12:03 +0200

    fix config

--- e2fsprogs_tests/config.sh
+++ e2fsprogs_tests/config.sh
@@ -2,7 +2,7 @@
 #
 # Test configuration
 #
-
+unset LANG LANGUAGE LC_ADDRESS LC_ALL LC_COLLATE LC_CTYPE LC_IDENTIFICATION LC_MEASUREMENT LC_MESSAGES LC_MONETARY LC_NAME LC_NUMERIC LC_PAPER LC_TELEPHONE LC_TIME PAGER
 FSCK="e2fsck"
 MKE2FS="mke2fs"
 DUMPE2FS="dumpe2fs"
@@ -18,27 +18,18 @@ RESIZE2FS_EXE="/sbin/resize2fs"
 RESIZE2FS="resize2fs"
 E2UNDO_EXE="/sbin/e2undo"
 E2UNDO="e2undo"
-SRCDIR="/usr/local/bin/e2fsprogs_tests/e2fsprogs-${PKG_VER}/build/tests"
-cmd_dir=$SRCDIR
+TEST_REL=../tests/progs/test_rel
+TEST_ICOUNT=../tests/progs/test_icount
+CRCSUM=../tests/progs/crcsum
 CLEAN_OUTPUT="sed -f $cmd_dir/filter.sed"
-##################################################################
-TEST_REL="${SRCDIR}/progs/test_rel"
-TEST_ICOUNT="${SRCDIR}/progs/test_icount"
-CRCSUM="${SRCDIR}/progs/crcsum"
 LD_LIBRARY_PATH=../lib:../lib/ext2fs:../lib/e2p:../lib/et:../lib/ss:${LD_LIBRARY_PATH}
 DYLD_LIBRARY_PATH=../lib:../lib/ext2fs:../lib/e2p:../lib/et:../lib/ss:${DYLD_LIBRARY_PATH}
 TZ=GMT0
 LC_ALL=C
 E2FSCK_CONFIG=/dev/null
-MKE2FS_CONFIG=./mke2fs.conf.in
+MKE2FS_CONFIG=./mke2fs.conf
 BLKID_FILE=/dev/null
 unset MKE2FS_FIRST_META_BG
 E2FSPROGS_SKIP_PROGRESS=yes
 EXT2FS_NO_MTAB_OK=yes
 E2FSPROGS_LIBMAGIC_SUPPRESS=yes
-##################################################################
-HTREE="y"
-QUOTA="y"
-DIFF_OPTS="u"
-SIZEOF_TIME_T="8"
-DD="dd"
--- e2fsprogs_tests/runtest.sh
+++ e2fsprogs_tests/runtest.sh
@@ -10,31 +10,16 @@ rlJournalStart
         rlRun "cd e2fsprogs-${PKG_VER}"
         rlRun "mkdir build && cd build"
         rlRun "../configure"
-        rlRun "make"
-        rlRun "cp -R -u ../tests/* ./tests"
-        rlRun "cd tests"
-        rlRun "chmod +x ./test_one.in"
     rlPhaseEnd
     rlPhaseStartTest
-        rlRun "touch /tmp/artifacts/report.log"
-        for test in $(ls -a | grep ^[dfijmrtu]_.); do
-            ./test_one.in $test
-            if [[ -f "$test".ok ]]
-            then
-                echo "$test PASSED" >> /tmp/artifacts/report.log
-            elif [[ -f "$test".failed ]]
-            then
-                echo "$test FAILED" >> /tmp/artifacts/report.log
-            else
-                echo "$test SKIPPED" >> /tmp/artifacts/report.log
-            fi
-        done
-        rlRun "n_passed=$(grep -w PASSED -c /tmp/artifacts/report.log)"
-        rlRun "n_failed=$(grep -w FAILED -c /tmp/artifacts/report.log)"
-        rlRun "n_skipped=$(grep -w SKIPPED -c /tmp/artifacts/report.log)"
-        rlRun 'brief_result="PASSED: ${n_passed} FAIILED: ${n_failed} SKIPPED: ${n_skipped}"'
-        rlRun 'echo $brief_result > /tmp/artifacts/brief_report.log'
-        rlRun 'grep "FAILED: 0" /tmp/artifacts/brief_report.log' 0
+        rlRun "make check"
+        #rlRun "touch /tmp/artifacts/report.log"
+        #rlRun "n_passed=$(grep -w PASSED -c /tmp/artifacts/report.log)"
+        #rlRun "n_failed=$(grep -w FAILED -c /tmp/artifacts/report.log)"
+        #rlRun "n_skipped=$(grep -w SKIPPED -c /tmp/artifacts/report.log)"
+        #rlRun 'brief_result="PASSED: ${n_passed} FAIILED: ${n_failed} SKIPPED: ${n_skipped}"'
+        #rlRun 'echo $brief_result > /tmp/artifacts/brief_report.log'
+        #rlRun 'grep "FAILED: 0" /tmp/artifacts/brief_report.log' 0
     rlPhaseEnd
     rlPhaseStartCleanup
     rlPhaseEnd
